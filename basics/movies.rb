d4e436778dbd7aa25abac8b5d298aab9b0d81b61
Timothy Durand <oatte@pm.me>
2024-03-02 11:50:57 -0600

    basics exercise 3

---
+++ basics/movies.rb
@@ -0,0 +1,7 @@
+movies = { galaxy_quest: 1999,
+          team_america: 2004,
+          nightmare_before_christmas: 1993 }
+
+puts movies[:galaxy_quest]
+puts movies[:team_america]
+puts movies[:nightmare_before_christmas]
